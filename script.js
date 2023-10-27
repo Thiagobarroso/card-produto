5f095d6ef80c176c1c9667e66a7cf76808386127
Thiagobarroso <thiagoo.silva@live.com>
2023-10-27 00:03:37 -0300

    feito alteracao para carregamento da imagem no javascript

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const selecionar360 = document.querySelectorAll(".mudar");
 const imagem1 = document.querySelector(".imagem1");
 const imagem2 = document.querySelector(".imagem2");
+
+const imagemSofaGif = new Image();
+imagemSofaGif.src = "/img/sofa.gif";
 
 imagem2.addEventListener("click", function () {
   this.classList.toggle("ativo");
 
   if (this.classList.contains("ativo")) {
-    // Trocar as imagens quando o elemento é ativado
     imagem1.src = "/img/sofa.gif";
     imagem2.src = "/img/Vector(fechar).png";
   } else {
-    // Restaurar as imagens originais quando o elemento é desativado
     imagem1.src = "/img/image 2.png";
     imagem2.src = "/img/Vector.png";
   }
